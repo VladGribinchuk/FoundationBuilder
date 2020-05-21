af1974537b583cbc035c36e7c46ecfb22dfc6491
Dima <okrdima@gmail.com>
2020-05-21 15:26:50 +0300

    Finish fuctions calulate, contains for 2D and 3D, add method isValid, write Unittests

---
+++ UnitTests/AABBTest.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "UnitTests.h"
+#include "../GeometryCore/include/Point.h"
+#include "../GeometryCore/include/AABB.h"
+#include "../GeometryCore/include/Polygon.h"
+
+using namespace geom_utils;
+
+DEFINE_TEST_CASE(Calculate) {
+
+        Polygon poly = { FPoint2D(1, 2), FPoint2D(3, 4), FPoint2D(5, 6) };
+        AABB2D aabb2d(poly);
+        FPoint2D expectedMaxAnswer(5, 6);
+        FPoint2D expectedMinAnswer(1, 2);
+        TEST_ASSERT((aabb2d.min() == expectedMinAnswer && aabb2d.max() == expectedMaxAnswer), "Failure");
+      
+       
+}
+DEFINE_TEST_CASE(isNotValid) {
+  
+    Polygon poly;
+    AABB2D aabb2d(poly);
+    TEST_ASSERT(!aabb2d.isValid(aabb2d), "AABB must be not valid!");
+}
+
+DEFINE_TEST_CASE(isValid) {
+    Polygon poly = { FPoint2D(1, 2), FPoint2D(3, 4), FPoint2D(5, 6) };
+    AABB2D aabb2d(poly);
+    TEST_ASSERT(aabb2d.isValid(aabb2d), "AABB must be valid!");
+}
+
+DEFINE_TEST_CASE(countainsFpoint2DInAABB) {
+
+    AABB2D aabb2d(FPoint2D(1, 2), FPoint2D(7, 7));
+    FPoint2D point2d(2, 4);
+    TEST_ASSERT(aabb2d.contains(point2d) == true, "Failure");
+}
+
+DEFINE_TEST_CASE(countainsFpoint3DInAABB) {
+
+    AABB3D aabb3d(FPoint3D(1, 1, 1), FPoint3D(4, 4, 4));
+    FPoint3D point3d(2, 2, 3);
+    TEST_ASSERT(aabb3d.contains(point3d) == true, "Failure");
+}
